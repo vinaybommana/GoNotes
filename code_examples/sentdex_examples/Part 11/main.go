507b7e850c2ec6a8914f44e70a7b5c4ae7baf870
Vinay.Bommana <vinay.bommana@opsramp.com>
2019-01-30 12:47:16 +0530

    Part 11

---
+++ code_examples/sentdex_examples/Part 11/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+/*
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/politics.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/opinions.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/local.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/sports.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/national.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/world.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/business.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/technology.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/lifestyle.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/entertainment.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/goingoutguide.xml
+</loc>
+</sitemap>
+</sitemapindex>
+*/
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	// function overwriting
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	// stringBody := string(bytes)
+	// fmt.Println(stringBody)
+	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	fmt.Println(s.Locations)
+
+}
