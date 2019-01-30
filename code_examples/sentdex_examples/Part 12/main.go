e536f0c8415e4317f0c0cf54b1f6cfec8da8bda5
Vinay.Bommana <vinay.bommana@opsramp.com>
2019-01-30 12:54:04 +0530

    added part 12

--- code_examples/sentdex_examples/Part 12/main.go
+++ code_examples/sentdex_examples/Part 12/main.go
@@ -1,9 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+/*
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/politics.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/opinions.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/local.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/sports.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/national.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/world.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/business.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/technology.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/lifestyle.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/entertainment.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/goingoutguide.xml
+</loc>
+</sitemap>
+</sitemapindex>
+*/
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	// function overwriting
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	// stringBody := string(bytes)
+	// fmt.Println(stringBody)
+	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	// fmt.Println(s.Locations)
+	for _, Location := range s.Locations {
+		fmt.Printf("%s\n", Location)
 	}
+
 }
