47beee4d3aa2929f361f0a1924f09a9b7bb80ab7
Vinay.Bommana <vinay.bommana@opsramp.com>
2019-03-19 18:15:30 +0530

    changes

--- code_examples/HackerRank-ProblemSolving/DiagonalDifference.go
+++ code_examples/HackerRank-ProblemSolving/DiagonalDifference.go
@@ -1,13 +1,10 @@
 package main
 
+// package main
+
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"math"
-	"os"
-	"strconv"
-	"strings"
 )
 
 // Complete the diagonalDifference function below.
@@ -43,57 +40,57 @@ func getOppositeDiagonalSum(ar [][]int32) int32 {
 	return oSum
 }
 
-func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+// func main() {
+// 	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
+// 	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+// 	checkError(err)
 
-	defer stdout.Close()
+// 	defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 1024*1024)
+// 	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
-	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
+// 	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+// 	checkError(err)
+// 	n := int32(nTemp)
 
-	var arr [][]int32
-	for i := 0; i < int(n); i++ {
-		arrRowTemp := strings.Split(readLine(reader), " ")
+// 	var arr [][]int32
+// 	for i := 0; i < int(n); i++ {
+// 		arrRowTemp := strings.Split(readLine(reader), " ")
 
-		var arrRow []int32
-		for _, arrRowItem := range arrRowTemp {
-			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
-			checkError(err)
-			arrItem := int32(arrItemTemp)
-			arrRow = append(arrRow, arrItem)
-		}
+// 		var arrRow []int32
+// 		for _, arrRowItem := range arrRowTemp {
+// 			arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+// 			checkError(err)
+// 			arrItem := int32(arrItemTemp)
+// 			arrRow = append(arrRow, arrItem)
+// 		}
 
-		if len(arrRow) != int(n) {
-			panic("Bad input")
-		}
+// 		if len(arrRow) != int(n) {
+// 			panic("Bad input")
+// 		}
 
-		arr = append(arr, arrRow)
-	}
+// 		arr = append(arr, arrRow)
+// 	}
 
-	result := diagonalDifference(arr)
+// 	result := diagonalDifference(arr)
 
-	fmt.Fprintf(writer, "%d\n", result)
+// 	fmt.Fprintf(writer, "%d\n", result)
 
-	writer.Flush()
-}
+// 	writer.Flush()
+// }
 
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
+// func readLine(reader *bufio.Reader) string {
+// 	str, _, err := reader.ReadLine()
+// 	if err == io.EOF {
+// 		return ""
+// 	}
 
-	return strings.TrimRight(string(str), "\r\n")
-}
+// 	return strings.TrimRight(string(str), "\r\n")
+// }
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+// func checkError(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
--- code_examples/kerningham/ch-1/echo-1/echo.go
+++ code_examples/kerningham/ch-1/echo-1/echo.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 )
 
+func pickingNumbers(a []int32) int32 {
+	// Write your code here
+	int32AsIntValues := make([]int, len(a))
+	for i, val := range a {
+		int32AsIntValues[i] = int(val)
+	}
+	sort.Ints(int32AsIntValues)
+
+	// maxLength
+	return 1
+}
 
 func main() {
 	var s, sep string
@@ -13,4 +25,5 @@ func main() {
 		sep = " "
 	}
 	fmt.Println(s)
+	pickingNumbers([]int32{4, 6, 5, 3, 3, 1})
 }
